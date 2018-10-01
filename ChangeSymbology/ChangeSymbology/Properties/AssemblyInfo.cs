80e13cdc20400aaed7b0623c86dc5840c8b30e64
AVWORLD\laur8722 <lboyd@esri.com>
2018-10-01 15:34:22 -0700

    Added Symbology SOE.

---
+++ ChangeSymbology/ChangeSymbology/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChangeSymbology")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChangeSymbology")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b6e55716-7723-4a61-ac5d-ea4979e6ead5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: ESRI.ArcGIS.SOESupport.AddInPackage("ChangeSymbology", "36c0e445-1c19-4760-9289-21e7ef6ffd07",
+    Author = "laur8722",
+    Company = "",
+    Date = "10/1/2018 3:05:50 PM",
+    Description = "",
+    TargetProduct = "Server",
+    TargetVersion = "10.6",
+    Version = "1.0")]
